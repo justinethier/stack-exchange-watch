cf14830b0d1b104a11c90470037a24fa4ef7345e
Justin Ethier <justin.ethier@gmail.com>
2014-08-01 16:18:29 -0400

    Migrated common define's up here

---
+++ config.h
@@ -0,0 +1,14 @@
+/**
+ * This file is part of stack-watch, a utility to monitor Stack Exchange.
+ * Copyright (C) 2014 Justin Ethier <justinethier@github>
+ *
+ * Please use github to submit patches and bug reports:
+ * https://github.com/justinethier/stack-exchange-watch
+ */
+#ifndef __CONFIG__
+#define __CONFIG__
+
+#define VERSION_STR "0.1.0"
+#define DEBUG 0
+
+#endif
